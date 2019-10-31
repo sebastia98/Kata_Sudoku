cab3872e72a9522c7ecf89182e6d5c7304ef489c
sebastia98 <sadrover@cifpfbmoll.eu>
2019-10-31 19:44:37 +0100

    check numero y casos test

---
+++ check_numero.py
@@ -0,0 +1,20 @@
+def check_numero(sudoku):
+    assert isinstance(sudoku, list)
+    copia_sudoku = sudoku[:]
+    for fila in sudoku:
+        for elemento in fila:
+            if isinstance(elemento, int) == False:
+                return False
+    assert copia_sudoku == sudoku
+    return True
+if __name__ == "__main__":
+    incorrect = [[1, 2, 'p'], 
+                [2, 3, 2],
+                [3, 1, 's']]
+    correct = [[1, 2, 3],
+                [2, 3, 1],
+                [3, 1, 2]]
+    assert check_numero(incorrect) == False
+    assert check_numero(correct) == True
+
+
--- check_sudoku.py
+++ check_sudoku.py
@@ -1,4 +1,5 @@
 from check_cuadrado import check_cuadrado
+from check_numero import check_numero
 
 def check_sudoku(sudoku):
-    return check_cuadrado(sudoku)
+    return check_cuadrado(sudoku) and check_numero(sudoku)
