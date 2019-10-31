eeab52c517479136cb546fc28f03f28a032b8713
sebastia98 <sadrover@cifpfbmoll.eu>
2019-10-31 19:14:11 +0100

    casos test

--- test_check_sudoku.py
+++ test_check_sudoku.py
@@ -1,38 +1,62 @@
-correct = [[1, 2, 3],
-           [2, 3, 1],
-           [3, 1, 2]]
+from check_cuadrado import chech_cuadrado
 
-incorrect = [[1, 2, 3, 4],
-             [2, 3, 1, 3],
-             [3, 1, 2, 3],
-             [4, 4, 4, 2]]
+def test_uno():
+    correct = [[1, 2, 3],
+               [2, 3, 1],
+               [3, 1, 2]]
+    assert check_cuadrado(correct) == True
 
-incorrect1 = [[1, 2, 3],
-              [2, 3, 1],
-              [2, 3, 1]]
 
-incorrect2 = [[1, 2, 3, 4],
-              [2, 3, 1, 2],
-              [4, 1, 2, 3],
-              [2, 3, 1, 4]]
+def test_dos():
+    incorrect = [[1, 2, 3, 4],
+                 [2, 3, 1, 3],
+                 [3, 1, 2, 3],
+                 [4, 4, 4, 2]]
+    assert check_cuadrado(incorrect) == False
 
-incorrect3 = [[1, 2, 3, 4, 5],
-              [2, 3, 1, 5, 6],
-              [4, 5, 2, 1, 3],
-              [3, 4, 5, 2, 1],
-              [5, 6, 4, 3, 2]]
+def test_tres():
+    incorrect1 = [[1, 2, 3],
+                  [2, 3, 1],
+                  [2, 3, 1]]
+    assert check_cuadrado(incorrect1) == False
 
-incorrect4 = [['a', 'b', 'c'],
-              ['b', 'c', 'a'],
-              ['c', 'a', 'b']]
+def test_cuatro():
+    incorrect2 = [[1, 2, 3, 4],
+                  [2, 3, 1, 2],
+                  [4, 1, 2, 3],
+                  [2, 3, 1, 4]]
+    assert chech_cuadrado(incorrect2) == False
 
-incorrect5 = [[1, 1.5],
-              [1.5, 1]]
+def test_quinto():
+    incorrect3 = [[1, 2, 3, 4, 5],
+                  [2, 3, 1, 5, 6],
+                  [4, 5, 2, 1, 3],
+                  [3, 4, 5, 2, 1],
+                  [5, 6, 4, 3, 2]]
+    assert chech_cuadrado(incorrect3) == False
 
-irregular = [[1, 2, 3],
-             [2, 3, 1]]
+def test_seis(): 
+    incorrect4 = [['a', 'b', 'c'],
+                  ['b', 'c', 'a'],
+                  ['c', 'a', 'b']]
+    assert chech_cuadrado(incorrect4) == False
 
-irregular2 = [[1, 2, 3],
-              [2, 3, 1],
-              [3, 1]]
-nuevo = [[]]
+def test_siete():
+    incorrect5 = [[1, 1.5],
+                  [1.5, 1]]
+    assert chech_cuadrado(incorrect5) == False
+
+def test_siete():
+    irregular = [[1, 2, 3],
+                 [2, 3, 1]]
+    assert chech_cuadrado(irregular) == False
+
+def test_ocho():
+    irregular2 = [[1, 2, 3],
+                  [2, 3, 1],
+                  [3, 1]]
+    assert chech_cuadrado(irregular2) == False
+
+def test_nueve():
+    nuevo = [[]]
+    assert chech_cuadrado(nuevo) == False
