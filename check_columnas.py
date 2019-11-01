1b77cb1702ff6deb2a4d3308a598b1fb1171352b
sebastia98 <sadrover@cifpfbmoll.eu>
2019-11-01 09:56:14 +0100

    check columna

--- check_columnas.py
+++ check_columnas.py
@@ -1,15 +1,23 @@
 def check_columnas(sudoku):
+    
     assert isinstance (sudoku, list)
+    copia_sudoku = sudoku[:]
     numero_maximo = len(sudoku)
-    fila = 1
-    for columna in range (numero_maximo):
-        for elemento_fila in range (1, numero_maximo):
-            if elemento_fila[columna][0] != elemento_fila[columna][fila]:
-                return False
-            fila += 1
+    columna = 0
+    for fila in range (1, numero_maximo):
+        if sudoku[columna][0] != sudoku[columna][fila]:
+            return False
+        columna += 1
+    return True
+    assert copia_sudoku == sudoku    
+ 
 
 if __name__ == "__main__":
     incorrect1 = [[1, 2, 3],
                   [2, 3, 1],
                   [2, 3, 1]]
     assert check_columnas(incorrect1) == False
+    correct = [[1, 2, 3],
+                [2, 3, 1],
+                [3, 1, 2]]
+    assert check_columnas(correct) == True
--- check_sudoku.py
+++ check_sudoku.py
@@ -1,6 +1,7 @@
 from check_cuadrado import check_cuadrado
 from check_numero import check_numero
 from check_filas import check_filas
+from check_columnas import check_columnas
 
 def check_sudoku(sudoku):
-    return check_cuadrado(sudoku) and check_numero(sudoku) and check_filas(sudoku)
+    return check_cuadrado(sudoku) and check_numero(sudoku) and check_filas(sudoku) and check_columnas(sudoku)
